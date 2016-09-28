ddce2390b83c2f071dbf323ccb97580948dad8c8
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-09-28 11:45:59 +0300

    clean files

---
+++ pascal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    char ch;  // character fetched from source
+
+    //--Check the command line arguments.
+    if (argc != 2) {
+	   std::cerr << "Usage: list <source file>" << std::endl;
+    }
+}

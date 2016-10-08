3eda04874190d88934d883150c98657443a6ca07
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-10-08 17:27:45 +0300

    improving source buffer

--- src/pascal.cpp
+++ src/pascal.cpp
@@ -7,20 +7,20 @@
 
 int main(int argc, char *argv[])
 {
-    // char ch;
+    char ch;
 
     if (argc != 2) {
 	   std::cerr << "Usage: pascal <source file>" << std::endl;
        abort(invalidCommandLineArgs);
     }
 
-    /*SourceBuffer sourceBuffer(argv[1]);
+    SourceBuffer sourceBuffer(argv[1]);
 
     do {
         ch = sourceBuffer.getChar();
-    } while (ch != eofChar);*/
+    } while (ch != eofChar);
 
-    SourceBuffer *sourceBuffer = new SourceBuffer(argv[1]);
+    /*SourceBuffer *sourceBuffer = new SourceBuffer(argv[1]);
     Parser parser(sourceBuffer);
-    parser.parse();
+    parser.parse();*/
 }

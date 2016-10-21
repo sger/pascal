e7ad3a12b7c0e6c59a01e693a1bc7a8d2423cacd
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-10-21 14:56:26 +0300

    fixes

--- src/string_token.cpp
+++ src/string_token.cpp
@@ -3,10 +3,39 @@
 #include <stdio.h>
 #include <string.h>
 
-void StringToken::getToken(TextInBuffer &buffer) {
+#include "list_buffer.h"
 
+void StringToken::getToken(TextInBuffer &buffer) {
+	char ch;
+	char *ps = tokenString;
+
+	*ps = '\'';
+
+	ch = buffer.getChar();
+
+	while(ch != eofChar) {
+		if (ch ==  '\'')
+		{
+			ch = buffer.getChar();
+
+			if (ch != '\'') break;
+		}
+		else if(ch == '\0') 
+		{
+			ch = ' ';
+		}
+
+		*ps++ = ch;
+		ch = buffer.getChar();
+	}
+
+	if (ch == eofChar) displayError(errorUnexpectedEndOfFile);
+	
+	*ps++ = '\'';
+	*ps = '\0';
 }
 
 void StringToken::print() const {
-
+	sprintf(list.text, "\t%-18s %-s", ">> string:", tokenString);
+	list.putLine();
 }

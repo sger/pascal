8d7ecc7146f961130acfe1d473f19c3d0abbd26c
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-10-27 12:45:46 +0300

    updates

--- src/symbol_table_node.h
+++ src/symbol_table_node.h
@@ -2,7 +2,39 @@
 #define symbol_table_h
 
 class SymbolTableNode {
+	SymbolTableNode *leftNode, *rightNode;
+	char *pSymbolString;
+	short symbolTableIndex;
+	short nodeIndex;
 
+	friend class SymbolTable;
+public:
+	float value;
+
+	SymbolTableNode(const char *pStr);
+	~SymbolTableNode();
+
+	SymbolTableNode *getLeftSubTree() const {
+		return leftNode;
+	}
+
+	SymbolTableNode *getRightSubTree() const {
+		return rightNode;
+	}
+
+	char *getSymbolString() const {
+		return pSymbolString;
+	}
+
+	short *getSymbolTableIndex() const {
+		return symbolTableIndex;
+	}
+
+	short *getNodeIndex() const {
+		return nodeIndex;
+	}
+
+	void print() const;
 };
 
 #endif

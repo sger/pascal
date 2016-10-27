d26b8f91a52ae6aa1477ff261c1be4c894c995c5
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-10-27 14:50:08 +0300

    updating symbol table

--- src/symbol_table.h
+++ src/symbol_table.h
@@ -6,12 +6,34 @@
 #include "symbol_table_node.h"
 
 class SymbolTable {
+	SymbolTableNode *rootNode;
+	short nodeCount;
+	short symbolTableIndex;
 public:
+	SymbolTable() {
+		rootNode = NULL;
+		nodeCount = 0;
+		symbolTableIndex = 0;
+	}
+
+	~SymbolTable() {
+		delete rootNode;
+	}
+
 	SymbolTableNode *search(const char *pString) const;
 	SymbolTableNode *enter(const char *pString);
 
+	SymbolTableNode *getRootNode() const {
+		return rootNode;
+	}
+
+	int getNodeCount() const {
+		return nodeCount;
+	}
+
 	void print() const {
 		std::cout << "print()" << std::endl;
+		rootNode->print();
 	}
 };
 

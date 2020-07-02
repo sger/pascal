23116c0d9da16a8fc3023eb2dece8954a8b66c62
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-07-02 15:49:41 +0300

    feature: update cmakelists for unit tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include "common.h"
 
 int main(int argc, char *argv[])
 {
---
+++ test/src/my_tests.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "common.h"
+#include "misc.h"
+
+TEST(testTimesThree, integerTests)
+{
+    EXPECT_EQ(0, 0);
+    EXPECT_EQ(36, 36);
+}
